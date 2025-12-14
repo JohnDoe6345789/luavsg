ae0a1c13fa55f4bfe772a87c7d494827e9f46123
Richard Ward <johndoe6345789@gmail.com>
2025-12-14 18:32:07 +0000

    compression fixes

---
+++ lib/bzip2/bzip2Config.cmake
@@ -0,0 +1,34 @@
+
+####### Expanded from @PACKAGE_INIT@ by configure_package_config_file() #######
+####### Any changes to this file will be overwritten by the next CMake run ####
+####### The input file was bzip2Config.cmake.in                            ########
+
+get_filename_component(PACKAGE_PREFIX_DIR "${CMAKE_CURRENT_LIST_DIR}/../../../" ABSOLUTE)
+
+macro(set_and_check _var _file)
+  set(${_var} "${_file}")
+  if(NOT EXISTS "${_file}")
+    message(FATAL_ERROR "File or directory ${_file} referenced by variable ${_var} does not exist !")
+  endif()
+endmacro()
+
+macro(check_required_components _NAME)
+  foreach(comp ${${_NAME}_FIND_COMPONENTS})
+    if(NOT ${_NAME}_${comp}_FOUND)
+      if(${_NAME}_FIND_REQUIRED_${comp})
+        set(${_NAME}_FOUND FALSE)
+      endif()
+    endif()
+  endforeach()
+endmacro()
+
+####################################################################################
+
+include("${CMAKE_CURRENT_LIST_DIR}/bzip2Targets.cmake")
+
+# Back-compat common imported target name used by FindBZip2.cmake users
+if(NOT TARGET BZip2::BZip2 AND TARGET bz2)
+  add_library(BZip2::BZip2 ALIAS bz2)
+endif()
+
+check_required_components(bzip2)
